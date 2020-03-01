4bbced46bcfc8c51bc37bbb6c021fb7cff8229ca
Xavier Le Cunff <lecunffxavier@gmail.com>
2020-03-01 22:24:53 +0100

    accurate time

--- timerText.js
+++ timerText.js
@@ -132,7 +132,7 @@ export default class TextTimeComponent extends React.Component<Default, Props, S
       : 0;
     const ellapsed = Math.floor((Date.now() - this.state.start) / 1000)
     const time = (this.props.reverseCount) ? ellapsed : this.state.time - 1;
-    const timeText = (this.props.reverseCount) ? secondsToHms(timeReverse) : secondsToHms(time);
+    const timeText = (this.props.reverseCount) ? secondsToHms(time) : secondsToHms(time);
     const callback = (time <= 0) ? this.props.onTimeElapsed : this.refreshTime;
     this.setState({
       ...getInitialStateText(this.props),
